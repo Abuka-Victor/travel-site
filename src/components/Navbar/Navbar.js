0126be43e6f2a7c6c2e7a4d9b8ebedc2ce8cdb77
Abuka-Victor <abukavictoro@gmail.com>
2022-08-31 16:28:50 +0100

    Init

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+export default function Navbvar() {
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container"></div>
+      </nav>
+    </>
+  );
+}
